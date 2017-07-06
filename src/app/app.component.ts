1796bcdf0895ed0c072a2901accc5db6736525c5
Poonam Ranawat <p.ranawat@easternenterprise.com>
2017-07-06 17:06:49 +0530

    http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Response} from '@angular/http';
+
+import {HttpService} from "./http.service";
+import {letProto} from "rxjs/operator/let";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  providers: [HttpService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  items: any[] = [];
+  asyncString = this.httpSvc.getData();
+  constructor(private httpSvc: HttpService) {}
+
+  ngOnInit() {
+    this.httpSvc.getData()
+      .subscribe(
+        (data: any) => console.log(data)
+      );
+  }
+
+  onSubmit(username: string, email: string) {
+    this.httpSvc.sendData({username: username, email: email})
+      .subscribe(
+        (data: any) => console.log(data),
+        (error: any)  => console.log(error)
+      );
+  }
+
+  onGetOwnData() {
+    this.httpSvc.getOwnData()
+      .subscribe(
+        data => {
+          const myArray = [];
+          for(let key in data) {
+            myArray.push(data[key]);
+          }
+          this.items = myArray;
+        }
+      );
+  }
 }
